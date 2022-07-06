082e88007517b97bd1be416fbb2fea9bac708cb4
shenjiahao <1843781563@qq.com>
2022-07-06 18:27:56 +0800

    修改stop.sh

--- fabric-realty/network/explorer/stop.sh
+++ fabric-realty/network/explorer/stop.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 docker-compose down -v
-
-peer chaincode instantiate -o orderer1.trustchain.com:7050 -C medicinechannel -n chaincode -l golang -v 1.0 -c '{"Args":["init"]}' -P "OR ('AHospitalMSP.peer', 'BHospitalMSP.peer')" --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/trustchain.com/orderers/orderer1.trustchain.com/msp/tlscacerts/tlsca.trustchain.com-cert.pem --tls
